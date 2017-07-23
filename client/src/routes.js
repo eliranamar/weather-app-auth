89da5aff58e299bcf571ab37a915fb90414dcc43
Steven Winston <swinston100@hotmail.com>
2017-07-23 11:06:11 +0300

    capitalised

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import weatherApp from './weatherApp';
+import WeatherApp from './WeatherApp';
 import About from './weatherApp/common/About';
 import Page404 from './weatherApp/common/404';
 import {Switch, Route} from 'react-router-dom';
@@ -7,7 +7,7 @@ import {Switch, Route} from 'react-router-dom';
 const Routesss = () => (
     <div className="container">
          <Switch>
-            <Route name="home" exact path='/' component={weatherApp} />
+            <Route name="home" exact path='/' component={WeatherApp} />
             <Route name="about" exact path='/about' component={About} />
             <Route path="*" component={Page404}/>
         </Switch>
